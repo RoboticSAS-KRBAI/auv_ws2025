446767270f0ff2929944f48b913f8129099e984c
RoboticSAS-KRBAI <krbaijuara@gmail.com>
2025-09-25 13:36:26 +0700

    25-09-2025

---
+++ auv_pkg/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'auv_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=False,
+    maintainer='techsas',
+    maintainer_email='techsas@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "test_node = auv_pkg.first_node:main",
+            "accumulator = auv_pkg.node_accumulator:main",
+            "guidance_teensy = auv_pkg.node_guidance_teensy:main",
+            'object_detection = auv_pkg.node_object_detection:main',
+            "pub_teensy = auv_pkg.publisher_teensy:main",
+        ],
+    },
+)
